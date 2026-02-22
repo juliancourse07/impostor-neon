b0b3087ca606c7c55bb47a5d31a6f4ba7b474b14
juliancourse07 <jecoronadog@uqvirtual.edu.co>
2026-02-22 22:10:41 +0000

    Play laugh sound every 54 seconds on home (after user arm)

--- src/App.tsx
+++ src/App.tsx
@@ -1068,7 +1068,8 @@ export default function App() {
               <div className="hero-kicker">PARTY • 1 DISPOSITIVO • NEÓN</div>
               <h1 className="hero-title">Impostor Neón</h1>
               <p className="hero-subtitle">
-                Hablen, engañen, voten. El caos es parte del juego no sean niñas.
+                Hablen, engañen, voten. 
+                El caos es parte del juego no sean niñas.
               </p>
 
               <div className="hero-actions">
